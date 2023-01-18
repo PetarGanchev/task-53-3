a82f72c1dfe222109a9a1c74433b41f47776c0b0
Petar Ganchev <PetarGanchev@users.noreply.github.com>
2023-01-18 10:40:24 +0000

    add data price

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-
+  var elements = document.getElementsByClassName('product')
+  let price = document.getElementsByClassName('price')
+  for (let i=0; elements.length>i; i++) {
+    elements[i].setAttribute('data-price',price[i].innerHTML)
+    console.log(price[i].innerHTML)
+  }
 });
